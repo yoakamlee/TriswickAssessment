49e4a61c8250e3c88f0a1e175bcd809aee3c2815
yoakamlee <yoakamleesoupen@gmail.com>
2024-09-05 23:50:45 +0200

    Created database context and its tables - yls

--- TriswickAssessment/Data/DataContext.cs
+++ TriswickAssessment/Data/DataContext.cs
@@ -1,6 +1,20 @@
-﻿namespace TriswickAssessment.Data
+﻿using System.Data.Entity;
+using TriswickAssessment.Models;
+
+namespace TriswickAssessment.Data
 {
-    public class DataContext
-    {
+    public class DataContext : DbContext
+        {
+        public DataContext() : base("DefaultConnection")
+            {
+
+            }
+        
+        //DB tables to be created based on models - yls
+        public DbSet<PostModel> posts {  get; set; }
+        public DbSet<UserModel> users { get; set; }
+        public DbSet<CommentsModel> comments { get; set; }
+        public DbSet<LikesModel> likes { get; set; }
     }
+   
 }
