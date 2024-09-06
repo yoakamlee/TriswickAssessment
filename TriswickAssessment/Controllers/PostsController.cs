08241e111078f03be5b3847de6de752e52ea101c
yoakamlee <yoakamleesoupen@gmail.com>
2024-09-06 23:38:22 +0200

    updeated PostController to get latest comments, added like get, need to comeple auth controller - yls

--- TriswickAssessment/Controllers/PostsController.cs
+++ TriswickAssessment/Controllers/PostsController.cs
@@ -34,6 +34,39 @@ namespace TriswickAssessment.Controllers
             return CreatedAtAction(nameof(GetPosts), new {id = post.Id});
         }
 
-        //TODO: Rest of post get/post request - yls
+        //UPDATE: uncompleted neeed to add authentication controller - yls
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PostModel>> GetPost(int id)
+        {
+            var post = await _context.Posts.Include(p => p.Comments).FirstOrDefaultAsync(p => p.Id == id);
+
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return post;
+        }
+
+
+        //TODO: add User auth - yls
+        [HttpPost("{id}/LikeCount")]
+        public async Task<IActionResult> LikePost(int id)
+        {
+            var post = await _context.Posts.FindAsync(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            var alreadyLiked = await _context.Likes.FirstOrDefaultAsync(l => l.PostId == id);
+
+            if(alreadyLiked != null)
+            {
+                return Conflict("You have already liked this post");
+            }
+
+            return NoContent();
+        }
     }
 }
