f88874848368fcf02a2e7efd47c09a988cededb4
yoakamlee <yoakamleesoupen@gmail.com>
2024-09-06 20:37:09 +0200

    Setup PostController basic get and post methods, TODO: rest of post methods - yls

--- TriswickAssessment/Controllers/PostsController.cs
+++ TriswickAssessment/Controllers/PostsController.cs
@@ -1,12 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TriswickAssessment.Data;
+using TriswickAssessment.Models;
 
 namespace TriswickAssessment.Controllers
 {
+    //probably change route name later? - yls 
+    [Route("api/[controller]")]
+    [ApiController]
     public class PostsController : Controller
     {
-        public IActionResult Index()
+        private readonly DataContext _context;
+
+        public PostsController(DataContext context)
         {
-            return View();
+            _context = context;
         }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PostModel>>> GetPosts()
+        {
+            //TODO: Get comments after making comment controller - yls 
+            var posts = await _context.Posts.ToListAsync();
+
+            return Ok(posts);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<PostModel>> CreatePost(PostModel post)
+        {
+            post.DateCreated = DateTime.Now;
+            post.DateUpdated = DateTime.Now;
+            _context.Posts.Add(post);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetPosts), new {id = post.Id});
+        }
+
+        //TODO: Rest of post get/post request - yls
     }
 }
